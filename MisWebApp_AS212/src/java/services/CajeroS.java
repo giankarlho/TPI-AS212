d4c7f75dcdf9aeeda079602ff26e04e591c8c8d8
Giancarlo Valencia Sotomayor <gvalencia@vallegrande.edu.pe>
2022-08-11 18:22:31 -0500

    Avance Cajero

---
+++ MisWebApp_AS212/src/java/services/CajeroS.java
@@ -0,0 +1,10 @@
+package services;
+
+public class CajeroS {
+    
+    public static String mensaje = "";
+    
+    public static void calcular(int tope, int monto){
+        
+    }
+}
--- MisWebApp_AS212/src/java/services/CaracolS.java
+++ MisWebApp_AS212/src/java/services/CaracolS.java
@@ -3,7 +3,8 @@ package services;
 public class CaracolS {
     
     public static String calcular(int altura, int sube, int resbala){
-        String rpta = "";
+        String rpta = validar(altura,sube,resbala);
+        if (rpta.length() != 0) return rpta;
         int avance =0, dias=0;
         while(avance < altura){
             avance += sube; // avance = avance + sube
@@ -13,5 +14,17 @@ public class CaracolS {
         }
         rpta = "Llegará en " + dias + " dias a salir del pozo";
         return rpta;
+    }
+    
+    public static String validar(int altura, int sube, int resbala){
+        String mensaje="";
+        if (resbala >= sube){
+            mensaje = "Ing. un nro. menor que la subida";
+        }else if (sube >= altura){
+            mensaje = "La altura debe ser mayor a la subida";
+        }else if (altura < 1){
+            mensaje = "la altura no puede ser negativa";
+        }
+        return mensaje;
     }
 }
